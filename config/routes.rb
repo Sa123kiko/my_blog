407635b7aed40262200572507ab339be4bbf6852
Sakiko <skk70437@gmail.com>
2019-10-13 16:52:33 +0900

    Add ルーティング
    ルーティングを書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#削除して下2行追記
+  resources :posts
+  root 'posts#index'
 end
